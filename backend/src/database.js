ef5a844fcab156c6c20d4f9bfa385b9d6cc5e49a
Matheus Leal <matheussleal98@gmail.com>
2022-09-05 13:36:22 -0300

    Criando projeto node, instalado express, nodemon point para listar estudante e buscar estudante na lista

---
+++ backend/src/database.js
@@ -0,0 +1,13 @@
+module.exports = [{
+        nome: 'Matheus Leal',
+        email: 'matheussleal98@gmail.com',
+        cpf: '333.112.191.21',
+        ra: '202014433',
+    },
+    {
+        nome: 'Taina Leal',
+        email: 'tainlg@gmail.com',
+        cpf: '344.132.181.21',
+        ra: '202013333',
+    },
+];
