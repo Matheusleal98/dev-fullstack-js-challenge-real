6f98cd1bef377969660e16ba1e2310d5ba3dba16
Matheus Leal <matheussleal98@gmail.com>
2023-01-30 17:43:41 -0300

    Configuramos o Knex na aplicação| Alteramos o endpoint de listar os usuários para pegar do banco | Atualizamos o endpoint de pegar um usuário no banco | Atualizamos endpoint de update do estudante para usar o banco

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,37 +1,49 @@
 const express = require('express');
+const knex = require("knex");
 var cors = require("cors");
 let database = require("./database");
+const knexConfigFile = require("../knexfile");
+const {response} = require("express");
+
 
 const app = express();
+
+app.database = knex(knexConfigFile.test);
+
 app.use(cors());
 app.use(express.json());
 
-app.get('/', function(req, res) {
+app.get('/', function (req, res) {
     res.send('Hello World');
 });
 
-app.get("/students/list/:searchQuery?", function(req, res) {
+app.get("/students/list/:searchQuery?", function (req, res) {
     let result = database;
     let search = req.params.searchQuery;
 
-    if(search) {
+    if (search) {
         search = search.toLowerCase();
         result = result.filter((student) => {
             return student.ra == search || student.nome.toLocaleLowerCase().indexOf(search) != -1 || student.cpf == search;
         })
     }
 
-    setTimeout(function() {
-        res.send(result);
-    }, 2000);
-
+    return app.database("students")
+        .select()
+        .then((data) => {
+            console.log(data);
+            res.send(data);
+        });
 });
 
-app.get("/students/find/:ra", function(req, res) {
-    const studentFound = database.find(function(student) {
-        return student.ra == req.params.ra;
-    })
-    res.send(studentFound);
+app.get("/students/find/:ra", function (req, res) {
+    return app.database("students")
+        .select()
+        .where({ra: req.params.ra})
+        .first()
+        .then((response) => {
+            res.send(response);
+        });
 });
 
 app.post("/students/save", (req, res) => {
@@ -41,27 +53,44 @@ app.post("/students/save", (req, res) => {
         email: req.body.email,
         cpf: req.body.cpf,
     });
-    res.send({ result: true, message: "Estudante cadastradado com sucesso!" });
+    res.send({result: true, message: "Estudante cadastradado com sucesso!"});
 });
 
-app.put("/students/edit/:ra", (req, res) => {
-    database = database.filter((student) => {
-        return student.ra != req.params.ra;
-    });
-    database.push({
-        nome: req.body.name,
-        ra: req.body.ra,
-        email: req.body.email,
-        cpf: req.body.cpf,
-    });
-    res.send({
-        result: true,
-        message: "O ALUNO FOI ATUALIZADO COM SUCESSO",
-    });
+app.put("/students/edit/:ra", async (req, res) => {
+
+    const userFound = await app.database("students").select()
+        .where({ra: req.params.ra})
+        .first()
+
+    if (!userFound) {
+        return res.status(400).send({
+            result: false,
+            message: "O estudante informado não existe!"
+        })
+    }
+    const studantUpdate = await app.database("students")
+        .update({
+            email: req.body.email,
+            nome: req.body.name
+        })
+        .where({
+            ra: req.params.ra
+        })
+    if (studantUpdate) {
+        res.send({
+            result: true,
+            message: `O usuário #${req.params.ra} foi atualizado com sucesso!`,
+        });
+    } else {
+        res.status(500).send({
+            result: false,
+            message: `Desculpa, mas não conseguimos atualizar o estudante.`,
+        });
+    }
 });
 
 app.delete("/students/delete/:ra", (req, res) => {
-    database = database.filter(function(student) {
+    database = database.filter(function (student) {
         return student.ra != req.params.ra;
     });
     console.log(database);
