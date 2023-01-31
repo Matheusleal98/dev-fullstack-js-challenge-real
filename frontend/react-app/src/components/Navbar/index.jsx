f8d6c0273e1c497a7aef12831fdac6827fdddbb1
Matheus Leal <matheussleal98@gmail.com>
2023-01-31 15:57:33 -0300

    Criado o componente NavBar e separado o css dele

---
+++ frontend/react-app/src/components/Navbar/index.jsx
@@ -0,0 +1,17 @@
+import "./style.css";
+const Navbar = () => {
+    return (
+        <nav className="main-nav">
+            <header>
+                Módulo Acadêmico
+            </header>
+            <ul className="nav-links">
+                <a className="nav-item" href="#">
+                    <li>Alunos</li>
+                </a>
+            </ul>
+        </nav>
+    );
+};
+
+export default Navbar;
